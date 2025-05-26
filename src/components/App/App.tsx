7b81f43fb30d687ce8d56603c6bb271a7d1623a9
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-05-26 22:13:33 +0300

    SearchBar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,1 +1,12 @@
 import css from "./App.module.css";
+import SearchBar from "../SearchBar/SearchBar";
+
+export default function App() {
+    const handleSearchBar = (data: string) => {
+
+    }
+
+    return <div className={css.app}>
+        <SearchBar onSubmit={handleSearchBar}/>
+    </div>
+}
